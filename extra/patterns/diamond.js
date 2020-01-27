dea9fa5b71a994d31bec92c82d237c8102e48449
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-01-27 14:17:55 +0530

    feat: diamond.js & equilateral.js add

---
+++ extra/patterns/diamond.js
@@ -0,0 +1,25 @@
+let s = Number(process.argv[2]);
+diamond(s);
+function diamond(o) {
+    if (o < 11 && o > 1) {
+        for (let j = 0; j < o; j++) {
+            let c = " ";
+            for (let k = 0; k < o - j; k++)
+                c = c + " ";
+            for (let l = 0; l <= j; l++)
+                c = c + "* ";
+            console.log(c);
+        }
+        for (let i = o - 1; i > 0; i--) {
+            let c = " ";
+            for (let j = 0; j < o - i; j++)
+                c = c + " ";
+            for (k = 0; k < i; k++)
+                c = c + " *";
+            console.log(c)
+        }
+    }
+    else {
+        console.log("wrong input");
+    }
+}
