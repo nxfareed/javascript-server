dbfacf8a088f65b24a11f63d2705740092109bb8
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-01-30 19:02:52 +0530

    feat:HTTP Status code and handlers

--- dist/Server.js
+++ dist/Server.js
@@ -1,19 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const bodyParser = require("body-parser");
+const errorHandler_1 = require("./libs/routes/errorHandler");
+const notFoundRoute_1 = require("./libs/routes/notFoundRoute");
 class Server {
     constructor(config) {
         this.config = config;
         this.bootstrap = () => {
             console.log('Inside Bootstrap');
+            this.initBodyParser();
             this.setupRoutes();
             return this;
+        };
+        this.initBodyParser = () => {
+            const { app } = this;
+            console.log('Inside init');
+            app.use(bodyParser.urlencoded({ extended: false }));
+            // parse application/json
+            app.use(bodyParser.json());
         };
         this.run = () => {
             const { app, config: { port } } = this;
             this.app.listen(this.config.port, (err) => {
                 if (err) {
-                    console.log("error");
+                    console.log('error');
                     throw err;
                 }
                 console.log('App is running successfully on port ' + port);
@@ -22,9 +33,16 @@ class Server {
         this.setupRoutes = () => {
             const { app } = this;
             this.app.get('/health-check', (req, res) => {
-                console.log("Inside health check");
+                console.log('Inside health check');
                 res.send('I am OK');
             });
+            app.use('/api', (req, res, next) => {
+                console.log('Inside Middleware');
+                console.log(req.body);
+                res.send(req.body);
+            });
+            app.use(notFoundRoute_1.default);
+            app.use(errorHandler_1.default);
             return this;
         };
         this.app = express();
--- src/Server.ts
+++ src/Server.ts
@@ -1,43 +1,66 @@
+
 import * as express from 'express';
 import Iconfig from './config/IConfig';
-
+import * as bodyParser from 'body-parser';
+import errorHandler from './libs/routes/errorHandler';
+import notFoundRoutes from './libs/routes/notFoundRoute';
+import { Request } from 'express';
 
 class Server {
-private app: express.Express
+    private app: express.Express;
 
-constructor(private config: Iconfig) {
-this.app = express();
-}
+    constructor(private config: Iconfig) {
+        this.app = express();
+    }
 
-bootstrap = () => {
-console.log('Inside Bootstrap')
-this.setupRoutes();
-return this;
-}
+    bootstrap = () => {
+        console.log('Inside Bootstrap');
+        this.initBodyParser();
+        this.setupRoutes();
+        return this;
+    }
 
+    initBodyParser = () => {
+        const { app } = this;
 
-run = () => {
-const { app, config: { port } } = this;
+        console.log('Inside init');
 
-this.app.listen(this.config.port, (err) => {
-if (err) {
-console.log("error");
-throw err;
-}
-console.log('App is running successfully on port ' + port);
-})
-}
+        app.use(bodyParser.urlencoded({ extended: false }));
 
-setupRoutes = () => {
-const { app } = this;
+        // parse application/json
+        app.use(bodyParser.json());
+    }
 
-this.app.get('/health-check', (req: express.Request, res: express.Response) => {
-console.log("Inside health check");
-res.send('I am OK');
-});
+    run = () => {
+        const { app, config: { port } } = this;
 
-return this;
-}
+        this.app.listen(this.config.port, (err) => {
+            if (err) {
+                console.log('error');
+                throw err;
+            }
+            console.log('App is running successfully on port ' + port);
+        });
+    }
+
+    setupRoutes = () => {
+        const { app } = this;
+
+        this.app.get('/health-check', (req: express.Request, res: express.Response) => {
+            console.log('Inside health check');
+            res.send('I am OK');
+        });
+
+        app.use('/api', (req, res, next) => {
+            console.log('Inside Middleware');
+            console.log(req.body);
+            res.send(req.body);
+        });
+
+        app.use(notFoundRoutes);
+        app.use(errorHandler);
+        return this;
+    }
 }
 
 export default Server;
