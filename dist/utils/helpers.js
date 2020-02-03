9d0f1181d8f630c72d88613923345df9aca90e8c
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-02-03 17:20:32 +0530

    refactor: Removing ignored files

--- dist/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateEmail(email) {
-    const regex = /([a-zA-Z0-9\+_.@])+@successive.tech/gm;
-    const result = regex.test(email);
-    return result;
-}
-exports.validateEmail = validateEmail;
-//# sourceMappingURL=helpers.js.map
