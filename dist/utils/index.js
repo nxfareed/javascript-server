2c420ede42cf56f5fe0a6d4fca902cfc2a393b4d
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-02-03 16:40:23 +0530

    refactor: Removing ignored files

--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var permissions_1 = require("./permissions");
-exports.hasPermissions = permissions_1.default;
-var validation_1 = require("./validation");
-exports.validateUser = validation_1.default;
-//# sourceMappingURL=index.js.map
