a5cd696310fc8583b39174d9679a7b48dbb6c6be
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-02-04 16:41:21 +0530

    refactor: Indentation of the files

--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -1,12 +1,10 @@
 const errorHandler = (err, req, res, next) => {
     console.log('Error', err);
     res.send({
-    error: err.error,
-    status: err.code,
-    message: err.error || err.message,
-    timestamp: new Date()
+        error: err.error,
+        status: err.code,
+        message: err.error || err.message,
+        timestamp: new Date()
     })
-    }
-    
-    export default errorHandler;
-    
+}
+export default errorHandler;
