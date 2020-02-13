c6bcf6e0b88af5592213a14b09f8971b364c53f7
nxfareed <60101382+nxfareed@users.noreply.github.com>
2020-02-13 18:46:38 +0530

    Update permission.ts

--- src/libs/routes/permission.ts
+++ src/libs/routes/permission.ts
@@ -2,7 +2,7 @@ import { permissions } from './constantsTrn';
 export default function hasPermissions(moduleName: string, role: string, permissionType: string): boolean {
 
     for (let i = 0; i < permissions[moduleName][permissionType].length; i++) {
-        if (permissions[moduleName][permissionType][i].match(role)) {
+        if (permissions[moduleName][permissionType][i]===(role)) {
             return true;
         }
     }
